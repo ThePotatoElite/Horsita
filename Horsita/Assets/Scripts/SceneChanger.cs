1111256293a4e938579598a3dac2312fe2f08285
Alon <alon@cloverbite.com>
2024-07-06 16:45:30 +0300

    Opening menu

---
+++ Horsita/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //loading?
+
+        SceneManager.LoadScene(1);
+    }
+}
